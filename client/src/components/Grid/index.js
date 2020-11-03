b288f1f146d929b1f16ce99ac3db1efd171dcae7
George F <64555318+GAFelton@users.noreply.github.com>
2020-11-02 16:34:50 -0800

    Fixed Linting errors.

--- client/src/components/Grid/index.js
+++ client/src/components/Grid/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 // Exporting the Container, Row, and Col components from this file
 
-// This Container component allows us to use a bootstrap container without worrying about class names
+// This Container component lets us use a bootstrap container without worrying about class names
 export function Container({ fluid, children }) {
   return <div className={`container${fluid ? "-fluid" : ""}`}>{children}</div>;
 }
@@ -14,15 +14,17 @@ export function Row({ fluid, children }) {
 
 // This Col component lets us size bootstrap columns with less syntax
 // e.g. <Col size="md-12"> instead of <div className="col-md-12">
+/* eslint-disable no-shadow */
 export function Col({ size, children }) {
   return (
     <div
       className={size
         .split(" ")
-        .map(size => "col-" + size)
+        .map((size) => `col-${size}`)
         .join(" ")}
     >
       {children}
     </div>
   );
 }
+/* eslint-enable no-shadow */
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const mongoose = require("mongoose");
 const routes = require("./routes");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -19,6 +20,6 @@ app.use(routes);
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, () => {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
