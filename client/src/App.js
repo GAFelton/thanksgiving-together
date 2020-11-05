5a4b7d52349c30efb2040a18a2a55fe11a2afc6b
Max Nicolai <maxvnicolai@gmail.com>
2020-11-05 14:38:44 -0800

    removed legacy/bootstrap code to resolve crash

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Books from "./pages/Books";
 import Nav from "./components/Nav";
 
 function App() {
   return (
     <div>
       <Nav />
-      <Books />
     </div>
   );
 }
