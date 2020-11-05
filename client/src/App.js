7760336b4a759cc611e9fc74529a72990de9650a
George F <64555318+GAFelton@users.noreply.github.com>
2020-11-05 14:59:52 -0800

    Added authentication notes to App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import Books from "./pages/Books";
 import Nav from "./components/Nav";
 
+// AUTHENTICATION NOTES
+// Wrap providers around MAIN app.
+// Logic for determing "is user logged in?" should all be in the auth context provider.
+// React Router ((user) ? Logged-in Routes : Logged-out Routes)
 function App() {
   return (
     <div>
