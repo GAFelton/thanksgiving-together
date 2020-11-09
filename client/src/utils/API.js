9e8da3987fcfd847161cbbbaeb1e763195dc565d
George F <64555318+GAFelton@users.noreply.github.com>
2020-11-09 11:07:36 -0800

    Changed axios pattern  for requests to 'API.{dbname}.{CRUDrequest} '.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,10 +1,42 @@
 import axios from "axios";
 
-export default {
+const API = axios.create({
+  baseURL: "/api/v1",
+});
+
+const family = {
+  get: (params) => API.get("/family", { params }),
+  update: (data, params) => API.put("/family", data, { params }),
+  create: (data) => API.post("/", data),
+  archive: (params) => API.put("/family/archive", { params }),
+};
+
+const users = {
+  comparePassword: (data) => API.post("/", data),
+  get: (params) => API.get("/user", { params }),
+  update: (data, params) => API.put("/user", data, { params }),
+  create: (data, params) => API.post("/user/family", data, { params }),
+  archive: (params) => API.put("/user/archive", { params }),
+};
+
+const recipes = {
+  get: (params) => API.get("/recipe", { params }),
+  update: (data, params) => API.put("/recipe", data, { params }),
+  create: (data, params) => API.post("/recipe/family", data, { params }),
+  archive: (params) => API.put("/recipe/archive", { params }),
+};
+
+const discussionTopics = {
   // Gets All Discussion Topics
-  getDiscussTopics() {
-    return axios.get("/api/discussiontopics");
-  },
+  get: () => API.get("/discussiontopics"),
+
+};
+
+export default {
+  family,
+  users,
+  recipes,
+  discussionTopics,
   // Gets all books
   getBooks() {
     return axios.get("/api/books");
