a3436b2589467cbe824f4933ee12d5dd87a3eb27
George F <64555318+GAFelton@users.noreply.github.com>
2020-11-09 12:14:51 -0800

    Removed all references to example books db, as well as books files.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -49,21 +49,4 @@ export default {
   users,
   recipes,
   discussionTopics,
-  // Example axios requests
-  // Gets all books
-  getBooks() {
-    return axios.get("/api/books");
-  },
-  // Gets the book with the given id
-  getBook(id) {
-    return axios.get(`/api/books/${id}`);
-  },
-  // Deletes the book with the given id
-  deleteBook(id) {
-    return axios.delete(`/api/books/${id}`);
-  },
-  // Saves a book to the database
-  saveBook(bookData) {
-    return axios.post("/api/books", bookData);
-  },
 };
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,9 @@
 const router = require("express").Router();
-const bookRoutes = require("./books");
 const discussTopicRoutes = require("./discussTopicBtn");
 const recipeRoutes = require("./recipe");
 const userRoutes = require("./user");
 const familyRoutes = require("./family");
 
-router.use("/books", bookRoutes);
 router.use("/discussiontopics", discussTopicRoutes);
 router.use("/recipe", recipeRoutes);
 router.use("/user", userRoutes);
