5a6d5b90fbf7a7483d96cb7c8f99a5201ab7f9c6
George F <64555318+GAFelton@users.noreply.github.com>
2020-11-08 17:36:22 -0800

    Added routes for family, recipe, and user queries.

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,24 +1,41 @@
-const { Recipe } = require("../models");
+const db = require("../models");
 
 // TODO: Set up routes. Remember that sorting by Family can help with querying.
 // Always sort by archived: false.
 module.exports = {
-  // TODO findById
-
+  // TODO findById "GET /api/recipe/:id"
+  findById(req, res) {
+    db.Recipe
+      .findById(req.params.id)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
   // TODO findAllIngredients
 
   // TODO findAllByAuthor
 
-  // TODO create (When creating, remember to add the recipe _id to the correct family document.)
-
-  // TODO update
-
-  // TODO archive
+  // create (adding recipe to correct family) "POST /api/recipe/:(family)id"
+  create(req, res) {
+    db.Recipe
+      .create(req.body)
+      .then(({ _id }) => db.Family.findOneAndUpdate({ _id: req.params.id },
+        { $push: { recipes: _id } }, { new: true }))
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  // update "PUT /api/recipe/:id"
+  update(req, res) {
+    db.Recipe
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  // archive "PUT /api/recipe/archive/:id"
   archiveRecipe(req, res) {
     // is req.params.id the correct way to get ID from React?
-    const { id } = req.params.id;
-    Recipe
-      .findByIdAndUpdate(id, { archived: true }, { new: true })
+    const { _id } = req.params.id;
+    db.Recipe
+      .findByIdAndUpdate(_id, { archived: true }, { new: true })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
