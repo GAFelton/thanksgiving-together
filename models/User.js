ba7194a0afbb0b1459dfd9f2453604b71dcde979
George F <64555318+GAFelton@users.noreply.github.com>
2020-11-08 17:26:25 -0800

    Added { select: false } to password field in schema. Will not show in user queries unless specified.

--- models/User.js
+++ models/User.js
@@ -9,7 +9,7 @@ const userSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true, index: { unique: true } },
-  password: { type: String, required: true },
+  password: { type: String, required: true, select: false },
   familyAdmin: { type: Boolean, required: true, default: false },
   archived: { type: Boolean, required: true, default: false },
 }, { toJSON: { virtuals: true } });
