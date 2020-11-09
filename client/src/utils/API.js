4c286fb516dce76bb5b994b2145c3df652698fef
George F <64555318+GAFelton@users.noreply.github.com>
2020-11-09 11:14:34 -0800

    Code comments explaining different params and data needs.

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,14 @@
 import axios from "axios";
 
+// All routes should follow the pattern 'API.dbname.CRUDrequest'.
+// Data should follow db schemas.
+
+// Creating an axios instance - more config params can  be added, for now baseURL makes code DRY.
 const API = axios.create({
   baseURL: "/api/v1",
 });
 
+// Family axios routes
 const family = {
   get: (params) => API.get("/family", { params }),
   update: (data, params) => API.put("/family", data, { params }),
@@ -11,7 +16,10 @@ const family = {
   archive: (params) => API.put("/family/archive", { params }),
 };
 
+// User axios routes
+// Note that for creating a new user, the param must be the FAMILY id.
 const users = {
+  // data for comparePassword must contain email and password
   comparePassword: (data) => API.post("/", data),
   get: (params) => API.get("/user", { params }),
   update: (data, params) => API.put("/user", data, { params }),
@@ -19,6 +27,8 @@ const users = {
   archive: (params) => API.put("/user/archive", { params }),
 };
 
+// Recipe axios routes
+// Note that for creating a new recipe, the param must be the FAMILY id.
 const recipes = {
   get: (params) => API.get("/recipe", { params }),
   update: (data, params) => API.put("/recipe", data, { params }),
@@ -26,17 +36,20 @@ const recipes = {
   archive: (params) => API.put("/recipe/archive", { params }),
 };
 
+// Discussion Topics axios routes
 const discussionTopics = {
   // Gets All Discussion Topics
   get: () => API.get("/discussiontopics"),
 
 };
 
+// Exporting API methods
 export default {
   family,
   users,
   recipes,
   discussionTopics,
+  // Example axios requests
   // Gets all books
   getBooks() {
     return axios.get("/api/books");
