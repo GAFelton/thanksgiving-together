8c038f59b5c5d086f3d45a736d4ba539d9edea15
Max Nicolai <maxvnicolai@gmail.com>
2020-11-14 11:38:16 -0800

    basic implementation of external zoom client complete

--- client/src/components/Zoom/index.js
+++ client/src/components/Zoom/index.js
@@ -1,4 +1,28 @@
-import ZoomWindow from "./zoomWindow";
-import ZoomControl from "./zoomControl";
+// import ZoomWindow from "./zoomWindow";
+// import ZoomControl from "./zoomControl";
+import React from "react";
 
-export { ZoomWindow, ZoomControl };
+// Defining functional style code here for now-
+// Messages from zoom client display correctly at at least this size
+const frameStyle = {
+  minWidth: "500px",
+  minHeight: "250px",
+};
+
+// Export embedded link to our zoom client
+// Next step is to move iframe to zoomWindow as components,
+// rewrite zoomControl to include button sending request to client
+// from our db
+function Zoom() {
+  return (
+    <>
+      <iframe
+        src="https://ttzoomclient.herokuapp.com/"
+        title="Zoom Client"
+        style={frameStyle}
+      />
+    </>
+  );
+}
+
+export default Zoom;
