32214dece4ca4429febc4359dcfba78a77583cb5
George F <64555318+GAFelton@users.noreply.github.com>
2020-11-16 19:47:04 -0800

    Added getMe route.

--- routes/api/user.js
+++ routes/api/user.js
@@ -6,13 +6,17 @@ const userController = require("../../controllers/userController");
 router.route("/")
   .post(userController.comparePassword);
 
+// Matches with "/api/${version}/user/me"
+router.route("/me")
+  .get(auth, userController.me);
+
 // Matches with "/api/${version}/user/:id"
 router.route("/:id")
   .get(auth, userController.findById)
   .put(auth, userController.update);
 
-// Matches with "/api/${version}/user/family/:(family)id"
-router.route("/family/:id")
+// Matches with "/api/${version}/user/family"
+router.route("/family")
   .post(userController.create);
 
 // Matches with "/api/${version}/user/archive/:id"
