170531104cad3e07d4339460355a19b9cb12672a
Max Nicolai <maxvnicolai@gmail.com>
2020-11-17 21:37:25 -0800

    added request to button, working. needs routing/db pull

--- client/src/components/Zoom/zoomControl.js
+++ client/src/components/Zoom/zoomControl.js
@@ -1,10 +1,34 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+// Get code could/ maybe should be moved to utils/api, but doing here for now
+// to avoid confusion/conflicts
+import axios from "axios";
 
-// Use state to track-joining, joined, default(unclicked)
+// Use state to track-joining, joined, default(as in, unclicked)
 //  Gets required information from app db and sends to Zoom client
 const sendJoinRequest = () => {
+  // Button fn test
   console.log("test");
+
+  // Prep data to send - values should come from db, const should likely be let
+  const data = JSON.stringify({ id: 7791079090, pwd: "Fyr5Xk", name: "Big Test" });
+
+  const config = {
+    method: "post",
+    url: "https://ttzoomclient.herokuapp.com/create",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data,
+  };
+
+  axios(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
 const ZoomControl = () => (
