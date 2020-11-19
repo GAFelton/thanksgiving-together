10ca4fefef2b6462560a0c859ba0750dea6c3dd0
Max Nicolai <maxvnicolai@gmail.com>
2020-11-18 18:26:32 -0800

    accessed/passing user info through zoom component, building out fns/routing

--- client/src/components/Zoom/index.js
+++ client/src/components/Zoom/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ZoomClient from "./zoomClient";
 import ZoomControl from "./zoomControl";
+import { useAuth } from "../AuthContext";
 
 // next step: rewrite zoomControl as button sending request to client
 // from our db, to follow zoom frame- maybe better done as entirely
@@ -9,10 +10,18 @@ import ZoomControl from "./zoomControl";
 // Exports a div containing our zoom client-these could be merged into a single component,
 // but this structure might make styling easier-
 function Zoom() {
+  // Get user info used in creating meeting to pass to zoom client router/app's zoomControl button
+  const { id, firstName, lastName } = useAuth().user;
+  console.log(id);
   return (
     <>
-      <ZoomClient />
-      <ZoomControl />
+      <ZoomClient
+        userId={id}
+      />
+      <ZoomControl
+        firstName={firstName}
+        lastName={lastName}
+      />
     </>
   );
 }
