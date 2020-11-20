33fc97f5534fe0c70d489fad7cbbc8756cd3458d
George F <64555318+GAFelton@users.noreply.github.com>
2020-11-19 23:47:11 -0800

    Wrote inviteButton Component - shows invite link.

---
+++ client/src/components/InviteButton/index.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import {
+  Button, Alert,
+} from "react-bootstrap";
+import API from "../../utils/API";
+import { useAuth } from "../AuthContext";
+import "./inviteButton.css";
+
+function InviteButton() {
+  const [inviteURL, setInviteURL] = useState();
+  const [show, setShow] = useState(false);
+
+  const { user } = useAuth();
+
+  React.useEffect(() => {
+    const { family } = user;
+    const baseURL = window.location.origin;
+
+    API.family.get(family)
+      .then((response) => {
+        const inviteLink = `${baseURL}/joinfamily/${response.data.roomCode}`;
+        setInviteURL(inviteLink);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  function handleClick() {
+    setShow(true);
+  }
+
+  return (
+
+    <>
+      <Button onClick={handleClick}>
+        Generate Invite Link
+      </Button>
+      { show ? (
+        <Alert className="mt2" style={{ width: "fit-content" }} variant="success">
+          {inviteURL}
+        </Alert>
+      ) : ""}
+    </>
+
+  );
+}
+
+export default InviteButton;
