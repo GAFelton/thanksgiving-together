7c10cf43a0ff21fb76db4bc085518cddc527ce9c
George F <64555318+GAFelton@users.noreply.github.com>
2020-11-19 22:35:02 -0800

    Included useEffect to set familyDetail to the invite code extracted from the invite link.

--- client/src/components/RegistrationForm/RegistrationForm.js
+++ client/src/components/RegistrationForm/RegistrationForm.js
@@ -6,7 +6,7 @@ import { useAuth } from "../AuthContext";
 
 // The registration form allows users to sign up for a new account.
 // It has functions for both creating a new family and for joining an existing one.
-function RegistrationForm(props) {
+function RegistrationForm({ match }, props) {
   // Define error handler fn for reference
   const { showError } = props;
   // These state parameters are mostly straightforward.
@@ -21,6 +21,18 @@ function RegistrationForm(props) {
     familyDetail: "",
     successMessage: null,
   });
+
+  // useEffect extracts invite code as soon as the page is loaded.
+  React.useEffect(() => {
+    console.log(match.params);
+    if (match.params) {
+      setState((prevState) => ({
+        ...prevState,
+        newFamily: false,
+        familyDetail: match.params.invitecode,
+      }));
+    }
+  }, []);
 
   const { handleLogin } = useAuth();
   // Handles state updates, accounts for either text input or the checkbox.
