d337db47ba9f64e946e9b34290aa8d450a8199e9
George F <64555318+GAFelton@users.noreply.github.com>
2020-12-08 12:59:41 -0800

    Added handleDelete logic, cleaned up RecipeListItem prop labels.

--- client/src/components/Recipes/Saved/index.js
+++ client/src/components/Recipes/Saved/index.js
@@ -3,21 +3,42 @@ import { Row, Col } from "react-bootstrap";
 import { RecipeList, RecipeListItem } from "../RecipeList";
 import API from "../../../utils/API";
 import { useAuth } from "../../AuthContext";
+import { ACCESS_TOKEN_NAME } from "../../../constants/apiConstants";
 
-function SavedTab() {
+function SavedTab({ showError }) {
   // Array to receive/store family recipes
   const [recipes, setRecipes] = useState([]);
 
   // Retrieve user's family ID
   const { family } = useAuth().user;
+  const storedJWT = localStorage.getItem(ACCESS_TOKEN_NAME);
 
   // On each load, get family's recipes from DB and save to recipes array
   useEffect(() => {
+    // eslint-disable-next-line no-use-before-define
+    getRecipesFromDB();
+  }, []);
+
+  function getRecipesFromDB() {
     API.family.get(family)
       .then((response) => {
         setRecipes(response.data.recipes);
       });
-  }, []);
+  }
+
+  async function handleDelete(id) {
+    try {
+      await API.recipes.archive(
+        { headers: { token: storedJWT } },
+        id,
+      );
+      getRecipesFromDB();
+      showError(null);
+    } catch (error) {
+      showError("An error occurred while deleting a recipe.");
+      console.log(error);
+    }
+  }
 
   return (
     <Row>
@@ -28,11 +49,11 @@ function SavedTab() {
           <RecipeList>
             {recipes.map((recipe) => (
               <RecipeListItem
-                key={recipe.title}
+                key={recipe.id}
                 title={recipe.title}
-                ingredients={recipe.ingredients}
                 href={recipe.src}
-                Thumbnail={recipe.photo}
+                thumbnail={recipe.photo}
+                handleDelete={() => handleDelete(recipe.id)}
               />
             ))}
           </RecipeList>
