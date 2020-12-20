dd148ae05287603507c6e26c31fe859a3f8df10e
George F <64555318+GAFelton@users.noreply.github.com>
2020-12-19 16:26:37 -0800

    Fixed import path for auth context, added ThemeProvider to App.js.

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import {
   Redirect,
 } from "react-router-dom";
 import Container from "react-bootstrap/Container";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import { AuthProvider } from "./contexts/AuthContext";
 import Header from "./components/Header/Header";
 import LoginForm from "./components/LoginForm/LoginForm";
 import RegistrationForm from "./components/RegistrationForm/RegistrationForm";
@@ -17,7 +19,6 @@ import Games from "./components/Games";
 import RecipesPage from "./pages/Recipes";
 import PrivateRoute from "./utils/PrivateRoute";
 import PublicRoute from "./utils/PublicRoute";
-import { AuthProvider } from "./components/AuthContext";
 import AlertComponent from "./components/AlertComponent/AlertComponent";
 
 // App() mainly contains the React router. The router is wrapped in an AuthProvider,
@@ -27,47 +28,49 @@ import AlertComponent from "./components/AlertComponent/AlertComponent";
 function App() {
   const [errorMessage, updateErrorMessage] = useState(null);
   return (
-    <AuthProvider>
-      <Router>
-        <div className="App">
-          <Header showError={updateErrorMessage} />
-          <Container fluid>
-            <Switch>
-              <PublicRoute path="/" exact>
-                <RegistrationForm showError={updateErrorMessage} />
-              </PublicRoute>
-              <PublicRoute exact path="/register">
-                <RegistrationForm showError={updateErrorMessage} />
-              </PublicRoute>
-              <Redirect from="/joinfamily/:invitecode" to="/register/:invitecode" />
-              <PublicRoute exact path="/register/:invitecode">
-                <RegistrationForm showError={updateErrorMessage} />
-              </PublicRoute>
-              <PublicRoute path="/login">
-                <LoginForm showError={updateErrorMessage} />
-              </PublicRoute>
-              <Route path="/about">
-                <About />
-              </Route>
-              <Route path="/how-to">
-                <How />
-              </Route>
-              <PrivateRoute path="/main">
-                <Main />
-              </PrivateRoute>
-              <PrivateRoute path="/games">
-                <Games />
-              </PrivateRoute>
-              <PrivateRoute path="/recipes">
-                <RecipesPage showError={updateErrorMessage} />
-              </PrivateRoute>
-            </Switch>
-          </Container>
-          {/* AlertComponent shows errors to user via the prop showError={updateErrorMessage} */}
-          <AlertComponent errorMessage={errorMessage} hideError={updateErrorMessage} />
-        </div>
-      </Router>
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <Router>
+          <div className="App">
+            <Header showError={updateErrorMessage} />
+            <Container fluid>
+              <Switch>
+                <PublicRoute path="/" exact>
+                  <RegistrationForm showError={updateErrorMessage} />
+                </PublicRoute>
+                <PublicRoute exact path="/register">
+                  <RegistrationForm showError={updateErrorMessage} />
+                </PublicRoute>
+                <Redirect from="/joinfamily/:invitecode" to="/register/:invitecode" />
+                <PublicRoute exact path="/register/:invitecode">
+                  <RegistrationForm showError={updateErrorMessage} />
+                </PublicRoute>
+                <PublicRoute path="/login">
+                  <LoginForm showError={updateErrorMessage} />
+                </PublicRoute>
+                <Route path="/about">
+                  <About />
+                </Route>
+                <Route path="/how-to">
+                  <How />
+                </Route>
+                <PrivateRoute path="/main">
+                  <Main />
+                </PrivateRoute>
+                <PrivateRoute path="/games">
+                  <Games />
+                </PrivateRoute>
+                <PrivateRoute path="/recipes">
+                  <RecipesPage showError={updateErrorMessage} />
+                </PrivateRoute>
+              </Switch>
+            </Container>
+            {/* AlertComponent shows errors to user via the prop showError={updateErrorMessage} */}
+            <AlertComponent errorMessage={errorMessage} hideError={updateErrorMessage} />
+          </div>
+        </Router>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
 
